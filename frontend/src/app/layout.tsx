1ac9849e93264583735bfdd8177c51a786c4cc4c
Faruq <Faruq@ssp.local>
2025-07-22 14:51:01 -0400

    removed unused declaration

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,7 +4,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Image from "next/image";
 import I18nProvider from "./i18n-provider";
 
 const geistSans = Geist({
@@ -35,16 +34,6 @@ export default function RootLayout({
         <I18nProvider>
         {/* Background Gradient Layer */}
         <div className="absolute inset-0 z-[-2] bg-gradient-to-br from-white via-blue-100 to-blue-600 opacity-40" />
-
-
-        {/* Star Watermark */}
-        {/* <Image
-          src="/assets/logos/favicon.png"
-          alt="SSP Watermark"
-          width={500}
-          height={500}
-          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-5 blur-sm z-[-1] pointer-events-none select-none"
-        /> */}
 
         {/* Main Content */}
         <main className="relative z-10">{children}</main>
