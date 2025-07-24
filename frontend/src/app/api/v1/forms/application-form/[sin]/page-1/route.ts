e9838f974bf2a3d1e8342dbb6fb53e27a7261795
ridoy <ridoy@sspgroup.com>
2025-07-24 17:20:41 -0400

    refactor: change GET function to arrow function syntax in application form page-1 route

--- frontend/src/app/api/v1/forms/application-form/[sin]/page-1/route.ts
+++ frontend/src/app/api/v1/forms/application-form/[sin]/page-1/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export const GET = () => {
   return NextResponse.json({ message: 'Dummy GET request successful!' });
 } 
  
