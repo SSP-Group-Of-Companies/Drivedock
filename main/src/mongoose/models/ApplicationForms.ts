0308af99426733d4407105257af2380b01d4cb3a
ridoy <ridoy@sspgroup.com>
2025-08-14 17:53:55 -0400

    test

--- main/src/mongoose/models/ApplicationForms.ts
+++ main/src/mongoose/models/ApplicationForms.ts
@@ -2,7 +2,6 @@ import { IApplicationFormDoc } from "@/types/applicationForm.types";
 import { Model, model, models } from "mongoose";
 import applicationFormSchema from "../schemas/applicationForm/applicationFormSchema";
 
-// test
 const ApplicationForm: Model<IApplicationFormDoc> = models.ApplicationForm || model<IApplicationFormDoc>("ApplicationForm", applicationFormSchema);
 
 export default ApplicationForm;
